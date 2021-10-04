228a1576b7c9438dce6431ea2e679ce9308c4bcb
Felipe Nunes <felipens0806@gmail.com>
2021-10-04 19:01:40 -0300

    New Images and files documentation

--- Src/module/TkinterFunctions.py
+++ Src/module/TkinterFunctions.py
@@ -21,6 +21,11 @@ except mysql.connector.Error as error:
         print(error)
 
 def send_expression_mysql(expression: str):
+    """Submit INSERT and DELETE query to Class table in database
+
+    Args:
+        expression (str): Expression write in MySQL sintax
+    """
     cursor = db_connection.cursor()
     cursor.execute(expression)
     db_connection.commit()
@@ -44,7 +49,6 @@ class ListBoxManipulation:
             many_items (bool, optional): True -> 2+ files to attach at the same time.
             False -> 1 file to attached. Defaults to False.
         """
-        
         if many_items:
             for i in value:
                 self.lst.insert(END, i)
@@ -62,10 +66,11 @@ class ListBoxManipulation:
         return tuple_lst.index(self.lst.get(ANCHOR))
 
     def delete_item(self, ascending: bool):
-        """Delete item of listbox
+        """Internal function used to delete item in listbox.
+        It's used in the up_item_list and down_item_list functions 
 
         Args:
-            ascending (bool): [description]
+            ascending (bool): True to up_item_list || False to down_item_list
         """
         selected_item = self.lst.curselection()
         if ascending:
@@ -76,6 +81,8 @@ class ListBoxManipulation:
                 self.lst.delete(i)
 
     def up_item_list(self):
+        """Command for button "↑" in the main window.
+        """
         value = self.lst.get(ANCHOR)
         pos = ListBoxManipulation(self.lst).index_item_selected()
         new_pos = pos - 1
@@ -84,18 +91,32 @@ class ListBoxManipulation:
 
 
     def down_item_list(self):
+        """Command for button "↓" in the main window.
+        """
         value = self.lst.get(ANCHOR)
         pos = ListBoxManipulation(self.lst).index_item_selected()
         new_pos = pos + 2
         self.lst.insert(new_pos, value)
         ListBoxManipulation(self.lst).delete_item(True)
     
-    def selectFiles(self):
+    def selectFiles(self) -> None: 
+        """
+
+        Returns:
+            None
+        """
         return askopenfilenames()
 
 class SendFiles:
 
-    def __init__(self, widget, class_name, master, *args):
+    def __init__(self, widget: Listbox, class_name: str, *widget_clear) -> None:
+        """Class constructor.
+
+        Args:
+            widget (tk.ListBox): Attached files listbox
+            class_name (str): Class name on WhatsApp
+            widget_clear (tk.widget): Widget to clear content
+        """
         self.values = widget.get(0, END)
         if not self.is_empty(class_name):
             start_browser()
@@ -106,15 +127,25 @@ class SendFiles:
                 else:
                     WANavagation(class_name).send_midia(i)
             quit_browser()
-            self.clear_widget(args)
+            self.clear_widget(widget_clear)
             showinfo(title='Envio feito com sucesso!', message=f'O envio das menssagens para o {class_name} foi realizado com sucesso!', )
         else:
             showwarning(title='Nenhuma turma selecionada', message='Nenhuma turma selecionada. O programa foi interrompido!')
 
-    def is_empty(self, widget):
-        return True if widget == '' else False
+    def is_empty(self, class_name: str):
+        """Analizes if str is empty or not
+
+        Args:
+            class_name (str): Name colected in 'Class Name' combobox 
+
+        Returns:
+            (bool): True if is empty || False if not empty
+        """
+        return True if class_name == '' else False
 
     def clear_widget(self, *widget):
+        """Clears widget data according to widget type
+        """
         j=0
         while j < len(widget[0]):
             str_widget = str(widget[0][j])
@@ -132,6 +163,11 @@ class SendFiles:
 class ClassManipulation:
 
     def __init__(self, master):
+        """Constructor Class
+
+        Args:
+            master (Tk): Tk object
+        """
         self.master = master
         self.root = Toplevel(self.master)
         self.lbf_class = LabelFrame(self.root, text='  TURMAS REGISTRADAS  ')
@@ -155,6 +191,8 @@ class ClassManipulation:
 
 
     def load_classes(self):
+        """Colects class names in localhost database
+        """
         cursor = db_connection.cursor()
         sql = 'SELECT name FROM classesName'
         cursor.execute(sql)
@@ -163,15 +201,21 @@ class ClassManipulation:
             self.lst_class.insert(END, name)
 
     def add_class(self):
+        """Adds name new class colected in Entry widget in listbox and database
+        """
         self.lst_class.insert(END, self.new_class.get())
         send_expression_mysql(f"INSERT INTO classesName(name) VALUES('{self.new_class.get()}')")
         self.new_class.delete(0, END)
     
     def delete_item(self):
+        """Deletes listbox selected name, of listbox and database
+        """
         item_delete = self.lst_class.get(ANCHOR)
         send_expression_mysql(f"DELETE FROM classesName WHERE name='{item_delete}'")
         item_delete = self.lst_class.curselection()
         self.lst_class.delete(item_delete)
     
     def save_changes(self):
+        """Destroy TopLevel object
+        """
         self.root.destroy()
--- test/test_TkinterGUI.py
+++ test/test_TkinterGUI.py
@@ -19,8 +19,12 @@ except mysql.connector.Error as error:
 combobox_value = ''
 
 class SelectClass:
+    def __init__(self, master: Tk=None) -> None:
+        """Constructor Class Frame
 
-    def __init__(self, master=None) -> None:
+        Args:
+            master (Tk, optional): Tk object. Defaults to None.
+        """
         self.master = master
         self.lbf_classes = LabelFrame(self.master, text='  ESCOLHA A TURMA  ')
         self.cb_classes = ttk.Combobox(self.lbf_classes, postcommand=self.load_classes, width=33)
@@ -31,6 +35,8 @@ class SelectClass:
         self.btn_new_class.grid(row=0, rowspan=2, column=2, sticky=E, padx=8, pady=10)
 
     def load_classes(self):
+        """Download class name of database
+        """
         db_connection = conect_db()
         self.list_classes = []
         cursor = db_connection.cursor()
@@ -41,11 +47,21 @@ class SelectClass:
         self.cb_classes.configure(values=self.list_classes)
 
     def combobox_value(self, event):
+        """Update global variable 'combobox_value'
+
+        Args:
+            event: Defaut Tk for creation events
+        """
         global combobox_value
         combobox_value = self.cb_classes.get()
 
 class Title:
-    def __init__(self, master) -> None:
+    def __init__(self, master=None) -> None:
+        """Constructor Windows Title
+
+        Args:
+            master (Tk, optional): Tk object. Defaults to None.
+        """
         self.master = master
         self.lb_title = Label(self.master, text='AUTOZAP', font='Verdana 20 bold')
         self.lb_title.grid(row=0,columnspan=10)
@@ -53,12 +69,17 @@ class Title:
 class AttachFiles:
 
     def __init__(self, master=None) -> None:
+        """Constructor Attached listbox, Textbox, Label name frames
+
+        Args:
+            master (Tk, optional): Tk object. Defaults to None.
+        """
         self.master = master
         #Listbox of attached files
         self.lbf_archive = LabelFrame(self.master, text='  ORDEM DE ENVIO  ')
         self.lst_archive = Listbox(self.lbf_archive, width=50, height=10, selectmode=SINGLE)
         self.lst_archive.bind('<<ListboxSelect>>', self.show_name_file)
-        self.btn_send = Button(self.lbf_archive, text='ENVIAR', width=7, command=lambda: SendFiles(self.lst_archive, combobox_value, self.master, self.lst_archive, self.lb_name_file, SelectClass().cb_classes))
+        self.btn_send = Button(self.lbf_archive, text='ENVIAR', width=7, command=lambda: SendFiles(self.lst_archive, combobox_value, self.lst_archive, self.lb_name_file, SelectClass().cb_classes))
         self.btn_new_archive = Button(self.lbf_archive, text='ANEXAR ARQUIVO', command=lambda: ListBoxManipulation(self.lst_archive).insert_listbox(ListBoxManipulation().selectFiles(), many_items=True), width=15) 
         self.btn_up = Button(self.lbf_archive, text='↑', width=5, command=lambda: ListBoxManipulation(self.lst_archive).up_item_list())
         self.btn_down = Button(self.lbf_archive, text='↓', width=5, command=lambda: ListBoxManipulation(self.lst_archive).down_item_list())
@@ -87,6 +108,11 @@ class AttachFiles:
         self.btn_delete.grid(row=0, column=2, columnspan=2, sticky=W, padx=11, pady=10)
 
     def show_name_file(self, event):
+        """Show name in label name
+
+        Args:
+            event: Defaut Tk for creation events
+        """
         name_file = self.lst_archive.get(ANCHOR)
         if name_file.count('/') > 0:
             pos_start = len(name_file) - name_file[::-1].index('/')
@@ -95,15 +121,20 @@ class AttachFiles:
             self.lb_name_file.configure(text=name_file)
         
 class Main:
-    def __init__(self, master) -> None:
+    def __init__(self, master=None) -> None:
+        """Constructor all main window
+
+        Args:
+            master (Tk, optional): Tk object. Defaults to None.
+        """
         SelectClass(master)
         Title(master)
         AttachFiles(master)
 
-
 def main_aplication():
+    """Defines basics window properties
+    """
     root = Tk()
-
     width= 720
     height = 360
     x = root.winfo_screenwidth()
